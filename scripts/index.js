9e0835eeac408cdd6b0b34115340f5e19a03be34
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-20 23:56:14 +0200

    feat: Añadir función que añade multimedia
    Añadir función que evalua el tipo de multimedia y lo añade al html con
    los atributos correctos

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,24 @@ function pintarTablaCanal(feed) {
     );
 }
 
+function anadirMultimedia(contenido, tipo) {
+    var resultado = "<td>";
+    if (tipo.substr(0, 5) === "video") {
+        resultado += "<video width='320' height='240' controls>" +
+        "<source src='" + contenido + "' type='" + tipo + "'>El teu navegador no soporta video</video>"
+    } else if (tipo.substr(0, 5) === "image") {
+        resultado += "<img src='" + contenido + "' width='100%' heigth='100%' alt='Imatge no disponible'/>"
+    }
+    resultado += "</td>"
+    return resultado;
+}
+
 function pintarTablaItems(items) {
     var contenido = "";
     $.each(items, function(index, item) {
         contenido += "<tr><td>" + item.title + "</td>" +
                      "<td>" + item.content + "</td>" +
-                     "<td><img src='" + item.enclosure.link + "' width='100%' heigth='100%' alt='No hi ha Imatge'/></td>" +
+                     anadirMultimedia(item.enclosure.link, item.enclosure.type) +
                      "<td>" + item.pubDate + "</td>" +
                      "<td><a href='" + item.link + "' class='badge badge-info'>Aqui</a></td></tr>";
     });
