e9e6e7fca6d498ec7a09be402e773221fb9c084e
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-22 19:25:24 +0200

    feat: Leer XML directamente sin API de terceros
    Leer el XML de canal RSS sin necesidad de la API de terceros, con dos
    funciones nuevas se convierte los datos del XML a formato JSON para
    poder seguir usando las funciones ya creadas

--- scripts/index.js
+++ scripts/index.js
@@ -39,9 +39,13 @@ parseando el json con el contenido
 function pintarTablaItems(items) {
     var contenido = "";
     $.each(items, function (index, item) {
+        let multimedia = "<td><p>No hi ha multimedia</p></td>";
+        if (typeof item.media != "undefined") {
+            multimedia = anadirMultimedia(item.media.link, item.media.type);
+        }
         contenido += "<tr><td>" + item.title + "</td>" +
             "<td>" + item.content + "</td>" +
-            anadirMultimedia(item.enclosure.link, item.enclosure.type) +
+            multimedia +
             "<td>" + item.pubDate + "</td>" +
             "<td><a href='" + item.link + "' class='badge badge-info'>Notícia</a></td></tr>";
     });
@@ -50,12 +54,56 @@ function pintarTablaItems(items) {
 }
 
 /*
+Aquesta funció crea un JSON amb els valors amb la informació del canal RSS
+*/
+function crearJsonCanal(data) {
+    let channel = data.getElementsByTagName("channel")[0];
+    console.log(channel.getElementsByTagName("title")[0].childNodes[0].nodeValue);
+    let feed = {
+        "title": channel.getElementsByTagName("title")[0].childNodes[0].nodeValue,
+        "description": channel.getElementsByTagName("description")[0].childNodes[0].nodeValue,
+        "link": channel.getElementsByTagName("link")[0].childNodes[0].nodeValue
+    };
+
+    return feed;
+}
+
+/*
+Aquesta funció crea un JSON amb els valors de items del XML
+*/
+function crearJsonItems(data) {
+    let channel = data.getElementsByTagName("channel")[0];
+    let items = [];
+    let itemsXML = channel.getElementsByTagName("item");
+    $.each(itemsXML, function(index, item) {
+        let itemObjeto = {};
+        itemObjeto.title = item.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+        itemObjeto.link = item.getElementsByTagName("link")[0].childNodes[0].nodeValue;
+        itemObjeto.content = item.getElementsByTagName("content:encoded")[0].childNodes[0].nodeValue;
+        itemObjeto.pubDate = item.getElementsByTagName("pubDate")[0].childNodes[0].nodeValue;
+        if (typeof item.getElementsByTagName("media:content")[0] != "undefined") {
+            itemObjeto.media = {
+                "link": item.getElementsByTagName("media:content")[0].getAttribute("url"),
+                "type": item.getElementsByTagName("media:content")[0].getAttribute("type")
+            };
+        }
+
+        items.push(itemObjeto);
+    });
+    console.log(items[1]);
+    return items;
+}
+
+/*
 Esta función llama a las funciones que dibujan las tablas
 para poder separar las responsabilidades
 */
 function pintarTablas(data) {
-    pintarTablaCanal(data.feed);
-    pintarTablaItems(data.items);
+    let feed = crearJsonCanal(data);
+    let items = crearJsonItems(data);
+
+    pintarTablaCanal(feed);
+    pintarTablaItems(items);
 }
 
 /*
@@ -67,8 +115,12 @@ aunque solo permite hasta 10 items del canal RSS, por eso solo se muestran
 Este es el canal RSS original: https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada
 */
 function hacerPeticionRSS() {
-    $.ajax({
+    /*$.ajax({
         url: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ffeeds.elpais.com%2Fmrss-s%2Fpages%2Fep%2Fsite%2Felpais.com%2Fportada", success: pintarTablas
+    });*/
+
+    $.ajax({
+        url: "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada", success: pintarTablas
     });
 }
 
